c65742d30edb292eda4624d73b6288def64b27e9
vaisest <4550061+vaisest@users.noreply.github.com>
2023-10-28 00:52:10 +0300

    fix bicubic (for the most part)

--- scaling.py
+++ scaling.py
@@ -1,22 +1,24 @@
-from itertools import repeat
-import math
-import signal
-import cv2 as cv
-import numpy as np
-
-import pathlib
 import argparse
-from typing import cast, Callable
-from multiprocessing import Pool
-from functools import cache
-import time
 import concurrent.futures
+import math
+import pathlib
+import time
+from functools import cache
+from itertools import repeat
+from typing import Callable
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import skimage
 
 
-# bicubic convolution kernel aka catmull-rom spline
-# the value of a here is -0.5 as that was used in Keys' version
-def u(s: float, a: float = -0.5):
-    # print(s)
+# cache seems to speed this up enough to be worth
+@cache
+def u(s: float):
+    # bicubic convolution kernel aka catmull-rom spline
+    # the value of a here is -0.5 as that was used in Keys' version
+    a: float = -0.5
     s = abs(s)
     if 0 <= s < 1:
         return (a + 2) * s**3 - (a + 3) * s**2 + 1
@@ -28,9 +30,7 @@ def u(s: float, a: float = -0.5):
 def scale_channel(
     image: np.ndarray, ratio: float, H: int, W: int, u: Callable[[float], float]
 ) -> np.ndarray:
-    # image = image / 255
-    orig_min = image.min()
-    orig_max = image.max()
+    image = image.astype(np.float64) / 255
 
     # create new image
     new_H = math.floor(H * ratio)
@@ -53,94 +53,47 @@ def scale_channel(
             # to the original pixels on the left and above
             decy = iy - y
 
-            # original two-dimensional method from paper
-            # where we go through e.g. horizontally
-            #                                               (ix-1), ix, (ix+1), (ix+2)
-            # this means the distances used in the kernel will be
-            #                                           (decx-1), decx, (decx + 1), (decx + 2)
-            # [
-            #     print([(L - decx, M - decy) for L in range(-1, 2 + 1)])
+            # pix = sum(
+            #     sum(
+            #         image[iy + M, ix + L] * u(decx + L) * u(decy + M)
+            #         for L in range(-1, 2 + 1)
+            #     )
             #     for M in range(-1, 2 + 1)
-            # ]
-            big_image[j, i] = sum(
+            # )
+
+            pix = sum(
                 sum(
-                    image[iy + L, ix + L] * u(M - decx) * u(L - decy)
-                    for M in range(-1, 2 + 1)
+                    image[iy + M, ix + L] * u(decx + L) * u(decy + M)
+                    for L in range(-1, 2 + 1)
                 )
-                for L in range(-1, 2 + 1)
+                for M in range(-1, 2 + 1)
             )
-    current_min = big_image.min()
-    current_max = big_image.max()
 
-    big_image = (
-        ((big_image - current_min + orig_min) / current_max) * orig_max
-    ).astype(np.uint8)
+            # we limit results to [0, 1] because bicubic interpolation
+            # can produce pixel values outside the original range
+            big_image[j, i] = max(min(1, pix), 0)
+            # big_image[j, i] = pix
 
-    # x, y = i * (1 / ratio) + 2, j * (1 / ratio) + 2
-
-    # x1 = 1 + x - math.floor(x)
-    # x2 = x - math.floor(x)
-    # x3 = math.floor(x) + 1 - x
-    # x4 = math.floor(x) + 2 - x
-
-    # y1 = 1 + y - math.floor(y)
-    # y2 = y - math.floor(y)
-    # y3 = math.floor(y) + 1 - y
-    # y4 = math.floor(y) + 2 - y
-
-    # mat_l = np.matrix([[u(x1), u(x2), u(x3), u(x4)]])
-    # mat_m = np.matrix(
-    #     [
-    #         [
-    #             image[int(y - y1), int(x - x1)],
-    #             image[int(y - y2), int(x - x1)],
-    #             image[int(y + y3), int(x - x1)],
-    #             image[int(y + y4), int(x - x1)],
-    #         ],
-    #         [
-    #             image[int(y - y1), int(x - x2)],
-    #             image[int(y - y2), int(x - x2)],
-    #             image[int(y + y3), int(x - x2)],
-    #             image[int(y + y4), int(x - x2)],
-    #         ],
-    #         [
-    #             image[int(y - y1), int(x + x3)],
-    #             image[int(y - y2), int(x + x3)],
-    #             image[int(y + y3), int(x + x3)],
-    #             image[int(y + y4), int(x + x3)],
-    #         ],
-    #         [
-    #             image[int(y - y1), int(x + x4)],
-    #             image[int(y - y2), int(x + x4)],
-    #             image[int(y + y3), int(x + x4)],
-    #             image[int(y + y4), int(x + x4)],
-    #         ],
-    #     ]
-    # )
-    # mat_r = np.matrix([[u(y1)], [u(y2)], [u(y3)], [u(y4)]])
-    # big_image[j, i] = np.dot(np.dot(mat_l, mat_m), mat_r)
-
-    # print()
-    # big_image = cv.normalize(big_image, None, 255, 0, cv.NORM_MINMAX, cv.CV)
-    return big_image
+    return (big_image * 255).astype(np.uint8)
+    # return big_image
 
 
 def main(in_file: pathlib.Path, out_file: pathlib.Path, ratio: float):
     im_data = cv.imread(str(in_file))
+
+    # im_data = cv.cvtColor(im_data, cv.COLOR_RGB2BGR)
+
+    start = time.perf_counter()
+    print("Scaling image...")
 
     H, W, C = im_data.shape
 
     # pad by 2 px
     im_data_p = cv.copyMakeBorder(im_data, 2, 2, 2, 2, cv.BORDER_REFLECT)
 
-    # docs say this is slow, but it takes 4 ms for a 3840x2160 image
-    # which doesnt sound so slow
     channels = cv.split(im_data_p)
 
-    out_im_data: np.ndarray | None = None
-
-    start = time.perf_counter()
-    print("Scaling image...")
+    out_im_data: np.ndarray = np.zeros(1)
 
     # scaling images with big sizes can take a long time
     # and with how slow this implementation is
@@ -152,7 +105,6 @@ def main(in_file: pathlib.Path, out_file: pathlib.Path, ratio: float):
             list(
                 executor.map(
                     scale_channel,
-                    # [channels[0]],
                     channels,
                     repeat(ratio),
                     repeat(H),
@@ -162,34 +114,32 @@ def main(in_file: pathlib.Path, out_file: pathlib.Path, ratio: float):
             )
         )
 
-    # with Pool(C) as p:
-    #     result = p.starmap_async(
-    #         scale_channel,
-    #         zip(
-    #             [channels[0]],
-    #             repeat(ratio),
-    #             repeat(H),
-    #             repeat(W),
-    #             repeat(u),
-    #         ),
-    #     )
-    #     # https://www.reddit.com/r/learnpython/comments/152sfp8/how_to_stop_multiprocessingpool_with_ctrlc_python/
-    #     while not result.ready():
-    #         time.sleep(0.2)
-    #     out_im_data = cv.merge(result.get(10))
-    #     p.terminate()
-
-    print(im_data.min(), im_data.max(), im_data.dtype, im_data.shape)
-    print(out_im_data.min(), out_im_data.max(), out_im_data.dtype, out_im_data.shape)
-    proper = cv.resize(im_data, None, None, 2, 2, cv.INTER_CUBIC)
-    print(proper.min(), proper.max(), proper.dtype, proper.shape)
-
     print(f"Finished scaling in {time.perf_counter() - start} seconds")
-    cv.imshow("Original image", im_data)
-    cv.imshow("Scaled image", out_im_data)
-    cv.imshow("Proper image", proper)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+
+    # print(im_data.min(), im_data.max(), im_data.dtype, im_data.shape)
+    # print(out_im_data.min(), out_im_data.max(), out_im_data.dtype, out_im_data.shape)
+    # proper = cv.resize(im_data, None, None, ratio, ratio, cv.INTER_CUBIC)
+    # # proper = skimage.util.img_as_ubyte(
+    # #     skimage.transform.rescale(im_data, ratio, channel_axis=-1, order=3)
+    # # )
+    # # print(proper.min(), proper.max(), proper.dtype, proper.shape)
+
+    # fig, ax = plt.subplots(nrows=2, ncols=2)
+    # ax[0, 0].imshow(im_data)
+    # ax[0, 0].set_title("Original")
+    # ax[0, 1].imshow(out_im_data)
+    # ax[0, 1].set_title("My scale")
+    # ax[1, 0].set_title("Proper")
+    # ax[1, 0].imshow(proper)
+    # print(cv.PSNR(out_im_data, proper))
+    # print(cv.PSNR(out_im_data, out_im_data))
+    # ax[1, 1].set_title("Diffy")
+    # # ldiffy = out_im_data.astype(np.float32) / 255 - proper.astype(np.float32) / 255
+    # diffy = cv.absdiff(out_im_data, proper)
+
+    # # print(diffy.min(), diffy.max(), diffy.dtype, diffy.shape)
+    # ax[1, 1].imshow(diffy)
+    # plt.show()
 
 
 if __name__ == "__main__":
