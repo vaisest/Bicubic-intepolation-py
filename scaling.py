18e9a1f9c9b679c7010a17daef7c0855a51be8b6
Turtvaiz <4550061+vaisest@users.noreply.github.com>
2023-10-28 19:34:53 +0300

    improve performance minimally

--- scaling.py
+++ scaling.py
@@ -38,28 +38,19 @@ def scale_channel(
     big_image = np.zeros((new_H, new_W))
 
     for j in range(new_H):
+        # scale new image's coordinate to be in old image
+        y = j * (1 / ratio) + 2
+        # we separate x and y to integer and fractional parts
+        iy = int(y)
+        # ix and iy are essentially the closest original pixels
+        # as all the old pixels are in integer positions
+        # decx and decy as the fractional parts are then the distances
+        # to the original pixels on the left and above
+        decy = iy - y
         for i in range(new_W):
             x = i * (1 / ratio) + 2
-            y = j * (1 / ratio) + 2
-
-            # print(x, y, x - int(x), y - int(y))
-            # we separate x and y to integer and fractional parts
             ix = int(x)
             decx = ix - x
-            # ix and iy are essentially the closest original pixels
-            # as all the old pixels are in integer positions
-            iy = int(y)
-            # decx and decy as the fractional parts are then the distances
-            # to the original pixels on the left and above
-            decy = iy - y
-
-            # pix = sum(
-            #     sum(
-            #         image[iy + M, ix + L] * u(decx + L) * u(decy + M)
-            #         for L in range(-1, 2 + 1)
-            #     )
-            #     for M in range(-1, 2 + 1)
-            # )
 
             pix = sum(
                 sum(
@@ -72,16 +63,15 @@ def scale_channel(
             # we limit results to [0, 1] because bicubic interpolation
             # can produce pixel values outside the original range
             big_image[j, i] = max(min(1, pix), 0)
-            # big_image[j, i] = pix
 
     return (big_image * 255).astype(np.uint8)
-    # return big_image
 
 
 def main(in_file: pathlib.Path, out_file: pathlib.Path, ratio: float):
     im_data = cv.imread(str(in_file))
 
-    # im_data = cv.cvtColor(im_data, cv.COLOR_RGB2BGR)
+    # because plt uses rgb
+    im_data = cv.cvtColor(im_data, cv.COLOR_RGB2BGR)
 
     start = time.perf_counter()
     print("Scaling image...")
@@ -118,28 +108,51 @@ def main(in_file: pathlib.Path, out_file: pathlib.Path, ratio: float):
 
     # print(im_data.min(), im_data.max(), im_data.dtype, im_data.shape)
     # print(out_im_data.min(), out_im_data.max(), out_im_data.dtype, out_im_data.shape)
-    # proper = cv.resize(im_data, None, None, ratio, ratio, cv.INTER_CUBIC)
-    # # proper = skimage.util.img_as_ubyte(
-    # #     skimage.transform.rescale(im_data, ratio, channel_axis=-1, order=3)
-    # # )
+    proper_cv = cv.resize(im_data, None, None, ratio, ratio, cv.INTER_CUBIC)
+    proper_skimage = skimage.util.img_as_ubyte(
+        skimage.transform.rescale(im_data, ratio, channel_axis=-1, order=3)
+    )
     # # print(proper.min(), proper.max(), proper.dtype, proper.shape)
 
-    # fig, ax = plt.subplots(nrows=2, ncols=2)
-    # ax[0, 0].imshow(im_data)
-    # ax[0, 0].set_title("Original")
-    # ax[0, 1].imshow(out_im_data)
-    # ax[0, 1].set_title("My scale")
-    # ax[1, 0].set_title("Proper")
-    # ax[1, 0].imshow(proper)
-    # print(cv.PSNR(out_im_data, proper))
-    # print(cv.PSNR(out_im_data, out_im_data))
-    # ax[1, 1].set_title("Diffy")
-    # # ldiffy = out_im_data.astype(np.float32) / 255 - proper.astype(np.float32) / 255
-    # diffy = cv.absdiff(out_im_data, proper)
+    fig, ax = plt.subplots(nrows=4, ncols=2)
+    ax[0, 0].imshow(im_data)
+    ax[0, 0].set_title("Original")
+    ax[0, 1].imshow(out_im_data)
+    ax[0, 1].set_title("My scale")
 
-    # # print(diffy.min(), diffy.max(), diffy.dtype, diffy.shape)
-    # ax[1, 1].imshow(diffy)
-    # plt.show()
+    ax[1, 0].set_title("Proper OpenCV")
+    ax[1, 0].imshow(proper_cv)
+    ax[1, 1].set_title("Proper Skimage")
+    ax[1, 1].imshow(proper_cv)
+
+    print("my scale vs proper_cv psnr:", cv.PSNR(out_im_data, proper_cv))
+
+    ax[2, 0].set_title("Absdiff OpenCV vs My")
+    diffy_cv = cv.absdiff(out_im_data, proper_cv)
+    ax[2, 0].imshow(diffy_cv)
+    ax[2, 1].set_title("Absdiff Skimage vs My")
+    diffy_skimage = cv.absdiff(out_im_data, proper_skimage)
+    ax[2, 1].imshow(diffy_skimage)
+
+    ax[3, 1].set_title("Absdiff CV vs Skimage")
+    ax[3, 1].imshow(cv.absdiff(proper_cv, proper_skimage))
+    ax[3, 0].set_title("Absdiff CV vs Skimage")
+    ax[3, 0].imshow(cv.absdiff(proper_cv, proper_skimage))
+
+    print("diffy_cv", diffy_cv.min(), diffy_cv.max(), diffy_cv.dtype, diffy_cv.shape)
+    print(
+        "diffy_skimage",
+        diffy_skimage.min(),
+        diffy_skimage.max(),
+        diffy_skimage.dtype,
+        diffy_skimage.shape,
+    )
+    print(
+        "proper_skimage vs proper_opencv psnr:",
+        cv.PSNR(out_im_data, proper_cv),
+        cv.absdiff(proper_cv, proper_skimage).max(),
+    )
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -148,8 +161,8 @@ if __name__ == "__main__":
         description="This program implements the bicubic convolution interpolation image scaling algorithm.",
     )
     arg_parser.add_argument("in_file", type=pathlib.Path)
-    arg_parser.add_argument("out_file", type=pathlib.Path)
     arg_parser.add_argument("scaling_ratio", type=float, nargs="?", default=2.0)
+    arg_parser.add_argument("out_file", type=pathlib.Path, nargs="?")
 
     args = arg_parser.parse_args()
 
